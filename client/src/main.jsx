1d969dd325b389bedd9cd07102578fb83a4fe3d7
ykkkykkkykkk <ukill4444@gmail.com>
2026-06-03 23:05:43 +0900

    fix: suppress automatic PWA install prompt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+window.addEventListener('beforeinstallprompt', (e) => e.preventDefault());
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
